9941f46a83169393c1ea75a999a1fd2d503f4110
nukucumber <nukucumber@ya.ru>
2025-05-21 22:41:46 +1000

    YACS another beginning

--- src/Services/CloudStorageService/Presentation/Program.cs
+++ src/Services/CloudStorageService/Presentation/Program.cs
@@ -3,5 +3,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
+app.MapGet("/", () =>
+{
+    var response = new { rootAnswer = "helloWorld" };
+    return response;
+});
+
 
 app.Run();
